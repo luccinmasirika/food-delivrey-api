f2f064b0178a991b4f5eed51891c62ee796cd67c
Luccin Masirika <luccinmasirika@gmail.com>
2021-07-30 14:18:45 +0100

    users

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -8,17 +8,18 @@ const {
   update,
   disableAnableUser,
   updateInfo,
+  readAllAdmin,
 } = require('../controllers/user.controller');
 const {
   updateUserValidator,
   updateClientValidator,
-  updateLivreurValidator
+  updateLivreurValidator,
 } = require('../validation/auth.validation');
 
 const { readAllLivreur } = require('../controllers/livreur.contoller');
 
 router.get('/user/:userId', read);
-router.get('/user/all/:userId', readAll);
+router.get('/read/all/user/:userId', readAll);
 router.put('/user/:userId', disableAnableUser);
 
 router.put('/profil/user/:userId', updateUserValidator, multer, update);
@@ -26,6 +27,7 @@ router.put('/profil/user/:userId', updateUserValidator, multer, update);
 router.put('/profil/livreur/:userId', updateLivreurValidator, multer, update);
 router.put('/info/livreur/:userId', updateLivreurValidator, multer, updateInfo);
 router.get('/read/all/livreur/:userId', readAllLivreur);
+router.get('/read/all/admin/:userId', readAllAdmin);
 
 router.put('/profil/client/:userId', updateClientValidator, multer, update);
 
